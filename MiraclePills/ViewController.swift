f5d5d1b23b2a2b61ae4e826a5cbe7985c6193fbc
Michael Tomcal <mtomcal@cbtnuggets.com>
2016-10-06 16:48:45 -0700

    Added handler for picker

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var titleLabel: UILabel?
     @IBOutlet weak var stateButton: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "California", "New York"]
+    
     @IBAction func onButton(_ sender: AnyObject) {
+        picker.isHidden = false
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.dataSource = self
+        picker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,6 +30,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateButton.setTitle(states[row], for: UIControlState.normal)
+        picker.isHidden = true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
 
 
 }
